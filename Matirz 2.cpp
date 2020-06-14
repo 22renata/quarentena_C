71b35e9742a9b5e8cd7a1fbe3970a7a7643c623f
renatita <30702989+22renata@users.noreply.github.com>
2020-06-14 11:47:17 -0300

    Add files via upload

---
+++ Matirz 2.cpp
@@ -0,0 +1,41 @@
+/* 
+MATRIZ
+Programa 02: Criar um algoritmo que leia os elementos de uma matriz inteira de 4 x 4 e
+imprime os elementos da diagonal principal. 
+
+AutorA: Renata Pereira Martins Gomes
+Data de Criação: 11/05/2020
+
+*/
+
+
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<locale.h>
+
+main()
+{	
+	srand(time(NULL));
+	int a, s, d, f=4;
+	int vet[f][f];
+	
+	setlocale(LC_ALL,"portuguese");
+	
+	
+	printf("A matriz é:\n");
+	for(a=0;a<f;a++)
+	{
+		for(s=0;s<f;s++)
+		{
+		
+		vet[a][s]= rand()%100;
+		printf("%i\t", vet[a][s]);
+		}
+		printf("\n");	
+	}
+	
+	printf("\n\nA diagonal principal é:\n");
+	for(d=0;d<f;d++) printf("%i\t", vet[d][d]);
+	
+}
